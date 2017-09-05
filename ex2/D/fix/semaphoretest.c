b3f66e9c80ace518dbbddfac6c10bc7600bffaa8
eirikjohannes <larsen.eirikj@gmail.com>
2017-09-05 13:32:39 +0200

    Done with ex2

---
+++ ex2/D/fix/semaphoretest.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdbool.h>
+pthread_t PIDARR[5];
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+
+bool running=1;
+int var1=0, var2=0;
+
+void* kjooor(void *arg){
+//	pthread_t ID=pthread_self();
+	while(running){
+		pthread_mutex_lock(&mutex);
+		var1=var1+1;
+		var2=var1;
+		pthread_mutex_unlock(&mutex);
+	}	
+        return 0;
+}
+void* kjor2(void *arg){
+	//lock mutex
+//	pthread_t ID=pthread_self();
+	for(int i=0;i<20;i++){
+		pthread_mutex_lock(&mutex);	
+		printf("Number 1 is %i, number 2 is %i\n",var1,var2);
+		pthread_mutex_unlock(&mutex);
+		usleep(100000);
+	}
+	running=0;
+	//unlock mutex
+	return 0;	
+}
+int main(int argc, char **argv){
+//	bool running=1;
+//	int var1=0, var2=0;
+	//sem_init(&semaphore,0,3);
+        pthread_create(&(PIDARR[0]),NULL,kjooor,NULL);
+        pthread_create(&(PIDARR[1]),NULL,kjor2,NULL);
+
+        pthread_join((PIDARR[0]),NULL);
+        pthread_join((PIDARR[1]),NULL);
+
+        return 0;
+}
+
